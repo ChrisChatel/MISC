87d1e98b4ff059dd1db3b2ad57a6aa4cb40bb530
Chris Chatel <chrisjchatel@gmail.com>
2025-02-21 14:37:40 -0800

    updated misc.ohm and parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,10 @@
 // The parse() function uses Ohm to produce a match object for a given
-// source code program, using the grammar in the file carlos.ohm.
+// source code program, using the grammar in the file misc.ohm.
 
 import * as fs from "node:fs";
 import * as ohm from "ohm-js";
 
-const grammar = ohm.grammar(fs.readFileSync("src/carlos.ohm"));
+const grammar = ohm.grammar(fs.readFileSync("src/misc.ohm"));
 
 // Returns the Ohm match if successful, otherwise throws an error
 export default function parse(sourceCode) {
